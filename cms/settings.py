0713963d3dfde43e112e583e0788d4dc172cab1f
Divyam <divi.pnp@gmail.com>
2024-09-09 22:29:41 +0530

    log file added

--- cms/settings.py
+++ cms/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,13 +77,14 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'astuteai_cms',
-        'USER':'root',
-        'PASSWORD':'admin',
+        'NAME': 'CMS',
+        'USER':'new_user',
+        'PASSWORD':'new_password',
         'HOST':'localhost',
         'PORT':'3306'
     }
 }
+
 
 
 # Password validation
