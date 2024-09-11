6d2e263725116e90e981ebf960782404a552e4f5
Rk-Krishna <142603393+Rk-Krishna@users.noreply.github.com>
2024-09-11 21:43:40 +0530

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cms_app'
+    'cms_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
